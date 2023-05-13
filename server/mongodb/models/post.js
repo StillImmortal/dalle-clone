e092ba29cc02dd950db18a733d351fe2facd8c31
Никита <immortallatrommi22@gmail.com>
2023-05-13 22:20:22 +0700

    Creating backend

---
+++ server/mongodb/models/post.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema, model, models } from "mongoose";
+
+const PostSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  prompt: {
+    type: String,
+    required: true
+  },
+  photo: {
+    type: String,
+    required: true
+  },
+})
+
+const Post = models.Post || model('Post', PostSchema)
+
+export default Post
+

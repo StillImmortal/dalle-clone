2f332df7cc62ffdc0798b7a0909a2247cd5dae87
Никита <immortallatrommi22@gmail.com>
2023-05-13 22:19:08 +0700

    Creating backend

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -29,11 +29,12 @@ const CreatePost = () => {
   }
 
   const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value})
+  }
 
-  } 
-
-  const handleSupriseMe = (e) => {
-    
+  const handleSurpriseMe = (e) => {
+    const randomPrompt = getRandomPrompt(form.prompt)
+    setForm({ ...form, prompt: randomPrompt })
   }
 
   return (
@@ -67,7 +68,7 @@ const CreatePost = () => {
             value={form.prompt}
             handleChange={handleChange}
             isSurpriseMe
-            handleSupriseMe={handleSupriseMe}
+            handleSurpriseMe={handleSurpriseMe}
           />
 
           <div className='relative bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center'>
